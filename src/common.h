25af7a60978f2900cd1c8e50820021ffcf392287
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-10-14 22:54:32 +0100

    common.h

---
+++ src/common.h
@@ -0,0 +1,12 @@
+//
+// Created by elliot alderson on 14/10/2023.
+//
+
+#ifndef LEAFSCRIPT_COMMON_H
+#define LEAFSCRIPT_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif //LEAFSCRIPT_COMMON_H
