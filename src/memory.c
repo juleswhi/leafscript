b5245f9ccfc883112a1edf9bb87e4ebe2fc90652
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-10-14 23:05:43 +0100

    Add memory

---
+++ src/memory.c
@@ -0,0 +1,17 @@
+//
+// Created by elliot alderson on 14/10/2023.
+//
+
+#include <stdlib.h>
+
+#include "memory.h"
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    if(newSize == 0) {
+        free(pointer);
+        return null;
+    }
+
+    void* result = realloc(pointer, newSize);
+    return result;
+}
---
+++ src/memory.h
@@ -0,0 +1,20 @@
+//
+// Created by elliot alderson on 14/10/2023.
+//
+
+#ifndef LEAFSCRIPT_MEMORY_H
+#define LEAFSCRIPT_MEMORY_H
+
+#include "common.h"
+
+// if less than a byte, set to that or else just double it
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+    (type*)reallocate(pointer, sizeof(type), * (oldCOunt), \
+        sizeof(type) * newCount))
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+
+#endif //LEAFSCRIPT_MEMORY_H
