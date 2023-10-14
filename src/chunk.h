d60bd685d0d5fcfd7ce6027a930fdbbecf8d2f10
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-10-14 23:00:26 +0100

    add chunk

---
+++ src/chunk.h
@@ -0,0 +1,24 @@
+//
+// Created by elliot alderson on 14/10/2023.
+//
+
+#ifndef LEAFSCRIPT_CHUNK_H
+#define LEAFSCRIPT_CHUNK_H
+
+#include "common.h"
+
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+
+void writeChunk(Chunk* chunk, uint8_t byte);
+
+#endif //LEAFSCRIPT_CHUNK_H
