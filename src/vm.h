f04d2636760db5b9ec484d4a340741ffd052b908
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-10-15 19:55:09 +0100

    Added the leaf machine

---
+++ src/vm.h
@@ -0,0 +1,32 @@
+//
+// Created by jules on 15/10/2023.
+//
+
+#ifndef LEAFSCRIPT_VM_H
+#define LEAFSCRIPT_VM_H
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
+
+#endif //LEAFSCRIPT_VM_H
